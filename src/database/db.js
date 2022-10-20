58e63b8f3782622f2bdb9b644fe091e53decd29e
joaofp <jvfpdaemoon1@gmail.com>
2022-10-20 16:38:51 -0300

    connection: ready;

--- src/database/db.js
+++ src/database/db.js
@@ -1,1 +1,12 @@
-import express from 'express';
+import pg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { Pool } = pg;
+
+const connection = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
+export default connection;
