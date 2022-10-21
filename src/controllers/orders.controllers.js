048771a1871fc5ab4d4195a66c416775a6cf7f79
joaofp <jvfpdaemoon1@gmail.com>
2022-10-21 18:54:59 -0300

    [GET]/orders: 90%

--- src/controllers/orders.controllers.js
+++ src/controllers/orders.controllers.js
@@ -7,11 +7,48 @@ async function createOrder(req, res) {
     const totalPrice = res.locals.totalPrice;
     try {
         await connection.query(`INSERT INTO orders ("clientId", "cakeId", quantity, "totalPrice") VALUES ($1, $2, $3, $4);`,
-        [clientId, cakeId, quantity, totalPrice]);
+            [clientId, cakeId, quantity, totalPrice]);
         return res.sendStatus(201);
     } catch (error) {
         return res.sendStatus(500);
     }
 }
 
-export { createOrder };
+async function getOrders(req, res) {
+    try {
+        const orders = (await connection.query(`SELECT * FROM orders`)).rows;
+        const ordersResponse = [];
+        await Promise.all(orders.map(async (order) => {
+            var client = (await connection.query(`SELECT * FROM clients WHERE id = $1;`, [order.clientId])).rows[0];
+            var cake = (await connection.query(`SELECT * FROM cakes WHERE id = $1;`, [order.cakeId])).rows[0];
+            ordersResponse.push({
+                client: {
+                    id: client.id,
+                    name: client.name,
+                    address: client.address,
+                    phone: client.phone
+                },
+                cake: {
+                    id: cake.id,
+                    name: cake.name,
+                    price: cake.price,
+                    description: cake.description,
+                    image: cake.image
+                },
+                orderId: order.id,
+                createdAt: order.createdAt,
+                quantity: order.quantity,
+                totalPrice: order.totalPrice
+            });
+        }));
+        return res.status(200).send(ordersResponse);
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+
+}
+
+export {
+    createOrder,
+    getOrders
+};
--- src/routers/orders.routers.js
+++ src/routers/orders.routers.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createOrder } from '../controllers/orders.controllers.js';
+import { createOrder, getOrders } from '../controllers/orders.controllers.js';
 import { ordersAuth } from '../middlewares/orders.middlewares.js';
 
 const orderRouter = express.Router();
 
 orderRouter.post('/order', ordersAuth, createOrder);
+orderRouter.get('/orders', getOrders);
 
 export { orderRouter };
