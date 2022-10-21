0f6395aee9996e433356193eb90a886cba0bb41b
joaofp <jvfpdaemoon1@gmail.com>
2022-10-21 14:07:09 -0300

    [POST]/clients: done;

--- src/middlewares/clients.middlewares.js
+++ src/middlewares/clients.middlewares.js
@@ -4,12 +4,12 @@ import { clientsSchema } from "../schemas/clientsSchema.js";
 async function clientAuth(req, res, next) {
     const { name, address, phone } = req.body;
     const validation = clientsSchema.validate({ name, address, phone });
-    if (!validation) {
+    if (validation.error) {
         return res.sendStatus(400);
     }
     try {
         const exist = await connection.query(`SELECT * FROM clients WHERE name = $1`, [name]);
-        if(exist.rows > 0){
+        if (exist.rows.length > 0) {
             return res.sendStatus(409);
         }
         next();
--- src/schemas/clientsSchema.js
+++ src/schemas/clientsSchema.js
@@ -3,6 +3,10 @@ import Joi from "joi";
 const clientsSchema = Joi.object({
     name: Joi.string().min(1).required().trim(),
     address: Joi.string().min(1).required().trim(),
+    phone: Joi.string()
+    .min(10)
+    .max(11)
+    .required()
 });
 
 export { clientsSchema };
