555d7791ed79b560741b2f9babe8449a8c5ff892
joaofp <jvfpdaemoon1@gmail.com>
2022-10-25 11:37:05 -0300

    minimum requiriments: done;

--- src/controllers/orders.controllers.js
+++ src/controllers/orders.controllers.js
@@ -1,15 +1,13 @@
 import connection from "../database/db.js";
-import dayjs from 'dayjs';
 
 async function createOrder(req, res) {
     const clientId = res.locals.clientExists
     const cakeId = res.locals.cakeExists
     const quantity = res.locals.quantity;
     const totalPrice = res.locals.totalPrice;
-    const createdAt = dayjs().format('DD-MM-YYYY HH:mm');
     try {
-        await connection.query(`INSERT INTO orders ("clientId", "cakeId", quantity, "totalPrice", "createdAt") VALUES ($1, $2, $3, $4, $5);`,
-            [clientId, cakeId, quantity, totalPrice, createdAt]);
+        await connection.query(`INSERT INTO orders ("clientId", "cakeId", quantity, "totalPrice") VALUES ($1, $2, $3, $4);`,
+            [clientId, cakeId, quantity, totalPrice]);
         return res.sendStatus(201);
     } catch (error) {
         return res.sendStatus(500);
